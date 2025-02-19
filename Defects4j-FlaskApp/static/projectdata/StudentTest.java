51009c8231125d288478c903e5a052ba6bea44f0
xaVIer999 <1201330@isep.ipp.pt>
2025-02-19 13:54:46 +0000

    chore: delete static view data

--- Defects4j-FlaskApp/static/projectdata/StudentTest.java
+++ Defects4j-FlaskApp/static/projectdata/StudentTest.java
@@ -1,3 +1,9 @@
-public class StudentTest {
-    
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+    }
 }
--- Defects4j-FlaskApp/static/projectdata/dev_suite.java
+++
@@ -1,3 +0,0 @@
-public class dev_suite {
-    
-}
