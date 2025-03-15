b1b537fc8124a5bb9fd0ae71b4e6462160fc92f9
xaVIer999 <1201330@isep.ipp.pt>
2025-03-15 20:43:18 +0000

    feat: add new project versions for gson and lang

--- Defects4j-FlaskApp/models/projects/Lang/Lang.py
+++ Defects4j-FlaskApp/models/projects/Lang/Lang.py
@@ -4,7 +4,7 @@ from helpers import file_helper as fh
 class Lang(Project):
     def __init__(self, version, tool):
         super().__init__("Lang", version, tool)
-        self.available_versions = ["53"]
+        self.available_versions = ["12","53"]
 
     def compile_with_defects4j(self):
         fh.comment_java_file(self.get_devsuite_path(), 96)
