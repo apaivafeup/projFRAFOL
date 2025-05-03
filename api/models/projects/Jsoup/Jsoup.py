5aecd8303ae9db8936340dcc620a668749ac5880
xavi-999 <xaviercosta192@gmail.com>
2025-05-03 15:44:49 +0100

    fix: remove unsupported projects jsoup versions

--- api/models/projects/Jsoup/Jsoup.py
+++ api/models/projects/Jsoup/Jsoup.py
@@ -3,7 +3,7 @@ from models.Project import Project
 class Jsoup(Project):
     def __init__(self, version, tool):
         super().__init__("Jsoup", version, tool)
-        self.available_versions = ["4", "9", "27"]
+        self.available_versions = ["27"]
 
 def make_jsoup(version, tool):
     return Jsoup(version, tool)
