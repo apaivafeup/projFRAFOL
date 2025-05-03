3ab2cfb2ac1676a67901809ea3e80ae35ffcf7b9
xavi-999 <xaviercosta192@gmail.com>
2025-05-03 17:17:49 +0100

    fix: remove support for lang53

--- api/models/projects/Lang/Lang.py
+++ api/models/projects/Lang/Lang.py
@@ -4,10 +4,10 @@ from helpers import file_helper as fh
 class Lang(Project):
     def __init__(self, version, tool):
         super().__init__("Lang", version, tool)
-        self.available_versions = ["12","53"]
+        self.available_versions = ["12"]
 
     def compile_with_defects4j(self):
-        fh.comment_java_file(self.get_devsuite_path(), 96)
+        #fh.comment_java_file(self.get_devsuite_path(), 96)
         super().compile_with_defects4j()
 
 def make_lang(version, tool):
--- client/src/components/DataTable/index.tsx
+++ client/src/components/DataTable/index.tsx
@@ -6,9 +6,9 @@ import "datatables.net-bs5/css/dataTables.bootstrap5.min.css";
 import "./DataTableStyle.css";
 import { useCurrentProject } from "../../context/currentProject";
 
-interface DataTableComponentProps {}
+interface DataTableComponentProps { }
 
-const DataTableComponent = ({}: DataTableComponentProps) => {
+const DataTableComponent = ({ }: DataTableComponentProps) => {
   const tableRef = useRef<HTMLTableElement | null>(null);
   const dataTableInstance = useRef<any>(null);
   const {
@@ -64,8 +64,8 @@ const DataTableComponent = ({}: DataTableComponentProps) => {
 
     const filteredData = excludeKilledMutants
       ? currentProject.mutantSheetData.filter(
-          (row) => !currentProject.killedMutants.includes(row[0]),
-        )
+        (row) => !currentProject.killedMutants.includes(row[0]),
+      )
       : currentProject.mutantSheetData;
 
     if (!filteredData.length) {
@@ -92,7 +92,7 @@ const DataTableComponent = ({}: DataTableComponentProps) => {
 
       row.forEach((cell) => {
         const cellElement = document.createElement("td");
-        if(!cell) {
+        if (!cell) {
           return
         }
         cellElement.textContent = cell.toString();
@@ -108,6 +108,16 @@ const DataTableComponent = ({}: DataTableComponentProps) => {
     excludeKilledMutants,
     setJumpToLineNumberOnClassUnderMutation,
   ]);
+
+
+
+  useEffect(() => {
+    window.alert = function () {
+      // Suppress alert or log it instead
+      console.warn("Suppressed alert:", ...arguments);
+    };
+  }, []);
+
 
   if (!currentProject) {
     return <div>Loading...</div>;
